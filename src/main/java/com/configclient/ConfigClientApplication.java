eb7d88790a6a5e526d8e419c25f088a2ac90a4dd
jyou <jyou@mo9.com>
2018-11-01 15:52:47 +0800

    first commit

---
+++ src/main/java/com/configclient/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(scanBasePackages = {"com.*"})
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
---
+++ src/main/java/com/configclient/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jyou on 2018/11/1.
+ *
+ * @author jyou
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Value("${test}")
+    private String test;
+}
